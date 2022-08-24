415fd70e6c8d8a88680576674a00237bf902ea0f
rahulavashist <rahulavashist@gmail.com>
2022-08-25 00:11:13 +0530

    JavaScript practice

---
+++ test.js
@@ -0,0 +1,40 @@
+// let details = {
+//     name:"Ram",
+//     class:10,
+//     rollNo:23,
+//     eyeColor:"pink",
+//     house:"tripti",
+//     getNameDes: function(){
+//       return  "my Name is Ram"
+//     }
+// }
+// console.log(details.getNameDes())
+//    let x= Object.values(details)
+//     console.log(x)
+//     let y= Object.keys(details)
+//     console.log(y.length)
+    // console.log(y[0]+"p")
+    // console.log(y[1]+"p")
+    // console.log(y[2]+"p")
+    // console.log(y[3]+"p")
+    // console.log(y[4]+"p")
+
+    // for(i=0;i<y.length;i++){
+    //     console.log(y[i]+ "ddd")
+    // }
+    
+
+
+    
+let str="HELLO THIS is rahul and i`m very cool person"
+
+console.log(str.length)
+console.log(str.slice(3,8))
+console.log(str.substring(5))
+
+
+console.log(str.substr(10))
+console.log(str.replace("hello","Hello"))
+console.log(str.toUpperCase())
+console.log(str.toLocaleLowerCase(0,5))
+console.log()
